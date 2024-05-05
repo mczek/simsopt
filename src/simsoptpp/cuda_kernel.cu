74dcfbff8bdd2034b35f56eb8f5ba020990a65c7
mczek <mczek@users.noreply.github.com>
2024-05-04 17:56:12 -0700

    move deriv calc to its own function

--- src/simsoptpp/cuda_kernel.cu
+++ src/simsoptpp/cuda_kernel.cu
@@ -196,12 +196,8 @@ void dshape(double x, double h, double* dshape){
 
 // }
 
-
-void trace_particle(particle_t& p, double* rrange_arr, double* zrange_arr, double* phirange_arr, double* quadpts_arr,
-                        double dt, double tmax, double m, double q){
-    double mu;
-    int nsteps = (int) (tmax / dt);
-    // std::cout << tmax << "\t" << dt << "\t" << nsteps << "\n";
+// out contains derivatives for x , y, z, v_par, and then norm of B and surface distance interpolation
+void calc_derivs(double* state, double* out, double* rrange_arr, double* zrange_arr, double* phirange_arr, double* quadpts_arr, double m, double q, double mu){
     double r_shape[4];
     double phi_shape[4];
     double z_shape[4];
@@ -220,182 +216,235 @@ void trace_particle(particle_t& p, double* rrange_arr, double* zrange_arr, doubl
     double z_grid_size = (zrange_arr[1] - zrange_arr[0]) / (zrange_arr[2]-1);
     
 
+    double x = state[0];
+    double y = state[1];
+    double z = state[2];
+    double v_par = state[3];
+    double v_perp = state[4];
+
+    std::cout << "load v_par " << v_par;
+
+    // magnetic field quad points are in cylindrical coordinates
+    double r = sqrt(x*x + y*y);
+    double phi = atan2(y, x);
+
+    // index into mesh to obtain nearby points
+    int i = (int) ((r - rrange_arr[0]) / r_grid_size) + 1;
+    int j = (int) ((z - zrange_arr[0]) / z_grid_size) + 1;
+    int k = (int) (phi / phi_grid_size) + 1;
+
+    std::cout << "indices: " << i << "\t" << r << "\t" << rrange_arr[0] << "\t" << r_grid_size << "\n";
+    std::cout << "position: " << x << "\t" << y << "\t" << z <<"\n";
+
+    // normalized positions in local grid wrt e.g. r at index i
+    int nr = rrange_arr[2];
+    int nphi = phirange_arr[2];
+    int nz = zrange_arr[2];
+    double r_rel = (r -  (rrange_arr[0] + i*r_grid_size)) / r_grid_size;
+    double z_rel = (z -  (zrange_arr[0] + j*z_grid_size)) / z_grid_size;
+    double phi_rel = (phi - (k*phi_grid_size)) / phi_grid_size;
+
+
+    std::cout << r << "\t" << -1*(r_rel*r_grid_size - r) << "\t" << r_grid_size << "\n";
+    std::cout << z << "\t" << -1*(z_rel*z_grid_size - z) << "\t" << z_grid_size << "\n";
+    std::cout << phi << "\t" << -1*(phi_rel*phi_grid_size - phi) << "\t" << phi_grid_size << "\n";
+    std::cout << "using index " <<  (i*nz*nphi + j*nphi + k) << "\n";
+    std::cout << quadpts_arr[4*(i*nz*nphi + j*nphi + k) + 3] << "\n";
+    // std::cout << "grid point found \n";
+
+    // std::cout << "r_rel " << r_rel << "\t" << z_rel << "\t" << phi_rel << "\n";
+
+    shape(r_rel, r_shape);
+    shape(z_rel, z_shape);
+    shape(phi_rel, phi_shape);
+
+
+    // std::cout <<"shape set \n";
+    // accumulate interpolation of B
+    B[0] = 0.0;
+    B[1] = 0.0;            
+    B[2] = 0.0;
+
+    // interpolate the distance to the surface
+    double surface_dist = 0.0;
+
+    // std::cout << "starting B accumulation\n";
+    // quad pts are indexed r z phi
+    bool is_lost = false;
+    for(int ii=0; ii<=3; ++ii){             
+        for(int jj=0; jj<=3; ++jj){                 
+            for(int kk=0; kk<=3; ++kk){
+                int wrap_k = ((k+kk-1) % nphi) + 1;
+
+                if ((i+ii >= 0 & i+ii < nr) & (j+jj >= 0 & j+jj < nz)){
+                    int start = 4*((i+ii)*nz*nphi + (j+jj)*nphi + (wrap_k));
+                    // std::cout << "start=" << start << "\t" << 4*nr*nz*nphi << "\n";
+                    B[0] += quadpts_arr[start]   * r_shape[ii]*z_shape[jj]*phi_shape[kk];
+                    B[1] += quadpts_arr[start+1] * r_shape[ii]*z_shape[jj]*phi_shape[kk];
+                    B[2] += quadpts_arr[start+2] * r_shape[ii]*z_shape[jj]*phi_shape[kk];
+
+                    // is_lost = is_lost || (quadpts_arr[start+3] < 0); 
+                    // std::cout << ii << "\t" << jj << "\t" << kk << "\n";
+                    // std::cout << "interp surface dist val: " << quadpts_arr[start+3] << "\n";
+                    surface_dist += quadpts_arr[start+3] * r_shape[ii]*z_shape[jj]*phi_shape[kk];
+                } else{
+                    // std::cout << "bad grid index for" << r << "\t" << phi << "\t" << z <<"\n"; 
+                }
+
+            }
+        }
+    }
+    // std::cout << "B interpolated \n";
+
+    // std::cout << "r=" << r << "\t" << x << "\t" << y << "\t" << p.v_par << "\t" << surface_dist << "\n";
+
+    // std::cout << "particle not lost \n";
+
+    //  Interpolate grad B: columns are partial deriv wrt r, z, phi, rows are entries of B
+    //  row major order
+    for(int ii=0; ii<9; ++ii){
+        grad_B[ii] = 0.0;
+    }
+    dshape(r_rel, r_grid_size, r_dshape);
+    dshape(phi_rel, phi_grid_size, phi_dshape);
+    dshape(z_rel, z_grid_size, z_dshape);
+
+    for(int ii=0; ii<=3; ++ii){             
+        for(int jj=0; jj<=3; ++jj){                 
+            for(int kk=0; kk<=3; ++kk){
+                int wrap_k = ((k+kk-1) % nphi) + 1;
+                if ((i+ii >= 0 & i+ii < nr) & (j+jj >= 0 & j+jj < nz)){
+                    int start = 4*((i+ii)*nz*nphi + (j+jj)*nphi + (wrap_k));
+                    // interpolate gradient for each entry of B, filling in each column of the gradient
+                    for(int l=0; l<3; ++l){
+                        double Bval = quadpts_arr[start+l];
+                        grad_B[3*l]   += Bval * r_dshape[ii]*z_shape[jj]*phi_shape[kk];
+                        grad_B[3*l+1] += Bval * r_shape[ii]*z_dshape[jj]*phi_shape[kk];
+                        grad_B[3*l+2] += Bval * r_shape[ii]*z_shape[jj]*phi_dshape[kk];
+                    }
+                }
+
+            }
+        }
+    }
+
+    // std::cout << "grad B interpolated \n";
+
+
+    // convert gradient from cylindrical (r, z, phi) to cartesian coordinates (x, y, z)
+    double c = cos(phi);
+    double s = sin(phi);
+
+
+    for(int l=0; l<3; ++l){ // iter over row
+        double dfdr = grad_B[3*l];
+        double dfdphi_divr = grad_B[3*l+2] / r;
+        
+        grad_B[3*l]   = c*dfdr - s*dfdphi_divr;
+        grad_B[3*l+2] = grad_B[3*l+1]; // z index changes
+        grad_B[3*l+1] = s*dfdr + c*dfdphi_divr;
+    }
+
+
+    // now compute derivatives
+
+    // std::cout << "starting updates \n";
+
+    double normB = sqrt(B[0]*B[0] + B[1]*B[1] + B[2]*B[2]);
+
+
+    // compute \nabla |B|
+    //  \nabla |B| = (\nabla B  B)(2 |B|)
+    nabla_normB[0] = (grad_B[0]*B[0] + grad_B[1]*B[1] + grad_B[2]*B[2]) / (2*normB);
+    nabla_normB[1] = (grad_B[3]*B[0] + grad_B[4]*B[1] + grad_B[5]*B[2]) / (2*normB);
+    nabla_normB[2] = (grad_B[6]*B[0] + grad_B[7]*B[1] + grad_B[8]*B[2]) / (2*normB);
+
+    // compute B \times \nabla |B|
+    cross_prod[0] = B[1]*nabla_normB[2] - B[2]*nabla_normB[1];
+    cross_prod[1] = B[2]*nabla_normB[0] - B[0]*nabla_normB[2];
+    cross_prod[2] = B[0]*nabla_normB[1] - B[1]*nabla_normB[0];
+
+    std::cout << "compute x deriv: " << v_par << "\t" << B[0] << "\t" << normB << "\t" << v_perp << "\t" << v_par << "\t" << cross_prod[0] << "\t" << m << "\t" << q << "\n";
+
+    out[0] = v_par * B[0]/normB + (0.5*pow(v_perp, 2) + pow(v_par, 2))*cross_prod[0] * m/(q*pow(normB, 3));
+    out[1] = v_par * B[1]/normB + (0.5*pow(v_perp, 2) + pow(v_par, 2))*cross_prod[1] * m/(q*pow(normB, 3));
+    out[2] = v_par * B[2]/normB + (0.5*pow(v_perp, 2) + pow(v_par, 2))*cross_prod[2] * m/(q*pow(normB, 3));
+
+    double BdotNablaNormB = B[0]*nabla_normB[0] + B[1]*nabla_normB[1] + B[2]*nabla_normB[2];
+    out[3] = -mu*BdotNablaNormB;
+    out[4] = normB;
+    out[5] = surface_dist;
+
+}
+
+
+void trace_particle(particle_t& p, double* rrange_arr, double* zrange_arr, double* phirange_arr, double* quadpts_arr,
+                        double dt, double tmax, double m, double q){
+    double mu;
+    int nsteps = (int) (tmax / dt);
+    double surface_dist;
+    // std::cout << tmax << "\t" << dt << "\t" << nsteps << "\n";
+    // double r_shape[4];
+    // double phi_shape[4];
+    // double z_shape[4];
+
+    // double r_dshape[4];
+    // double phi_dshape[4];
+    // double z_dshape[4];
+
+    // double B[3];
+    // double grad_B[9];
+    // double nabla_normB[3];
+    // double cross_prod[3];
+
+    // double r_grid_size = (rrange_arr[1] - rrange_arr[0]) / (rrange_arr[2]-1);
+    // double phi_grid_size = 2*M_PI / phirange_arr[2];
+    // double z_grid_size = (zrange_arr[1] - zrange_arr[0]) / (zrange_arr[2]-1);
+    
+
     double t = 0.0;
     // for(int time_step=0; time_step<nsteps; ++time_step){
+
+    double state[5];
+    state[0] = p.x;
+    state[1] = p.y;
+    state[2] = p.z;
+    state[3] = p.v_par;
+    state[4] = p.v_perp;
+
+    double derivs[5];
+
+    // dummy call to get norm B
+    calc_derivs(state, derivs, rrange_arr, zrange_arr, phirange_arr, quadpts_arr, m, q, -1);
+    mu = p.v_perp*p.v_perp/(2*derivs[4]);
+
     while(t < tmax){
         /*
         * Time step ODE
         */
-        double x = p.x;
-        double y = p.y;
-        double z = p.z;
+        state[0] = p.x;
+        state[1] = p.y;
+        state[2] = p.z;
+        state[3] = p.v_par;
+        state[4] = p.v_perp;
 
-        // magnetic field quad points are in cylindrical coordinates
-        double r = sqrt(x*x + y*y);
-        double phi = atan2(y, x);
+        calc_derivs(state, derivs, rrange_arr, zrange_arr, phirange_arr, quadpts_arr, m, q, mu);
 
-        // index into mesh to obtain nearby points
-        int i = (int) ((r - rrange_arr[0]) / r_grid_size) + 1;
-        int j = (int) ((z - zrange_arr[0]) / z_grid_size) + 1;
-        int k = (int) (phi / phi_grid_size) + 1;
-
-        std::cout << "indices: " << i << "\t" << r << "\t" << rrange_arr[0] << "\t" << r_grid_size << "\n";
-        std::cout << "position: " << x << "\t" << y << "\t" << z <<"\n";
-
-        // normalized positions in local grid wrt e.g. r at index i
-        int nr = rrange_arr[2];
-        int nphi = phirange_arr[2];
-        int nz = zrange_arr[2];
-        double r_rel = (r -  (rrange_arr[0] + i*r_grid_size)) / r_grid_size;
-        double z_rel = (z -  (zrange_arr[0] + j*z_grid_size)) / z_grid_size;
-        double phi_rel = (phi - (k*phi_grid_size)) / phi_grid_size;
-
-
-        std::cout << r << "\t" << -1*(r_rel*r_grid_size - r) << "\t" << r_grid_size << "\n";
-        std::cout << z << "\t" << -1*(z_rel*z_grid_size - z) << "\t" << z_grid_size << "\n";
-        std::cout << phi << "\t" << -1*(phi_rel*phi_grid_size - phi) << "\t" << phi_grid_size << "\n";
-        std::cout << "using index " <<  (i*nz*nphi + j*nphi + k) << "\n";
-        std::cout << quadpts_arr[4*(i*nz*nphi + j*nphi + k) + 3] << "\n";
-        // std::cout << "grid point found \n";
-
-        // std::cout << "r_rel " << r_rel << "\t" << z_rel << "\t" << phi_rel << "\n";
-
-        shape(r_rel, r_shape);
-        shape(z_rel, z_shape);
-        shape(phi_rel, phi_shape);
-
-
-        // std::cout <<"shape set \n";
-        // accumulate interpolation of B
-        B[0] = 0.0;
-        B[1] = 0.0;            
-        B[2] = 0.0;
-
-        // interpolate the distance to the surface
-        double surface_dist = 0.0;
-
-        // std::cout << "starting B accumulation\n";
-        // quad pts are indexed r z phi
-        bool is_lost = false;
-        for(int ii=0; ii<=3; ++ii){             
-            for(int jj=0; jj<=3; ++jj){                 
-                for(int kk=0; kk<=3; ++kk){
-                    int wrap_k = ((k+kk-1) % nphi) + 1;
-
-                    if ((i+ii >= 0 & i+ii < nr) & (j+jj >= 0 & j+jj < nz)){
-                        int start = 4*((i+ii)*nz*nphi + (j+jj)*nphi + (wrap_k));
-                        // std::cout << "start=" << start << "\t" << 4*nr*nz*nphi << "\n";
-                        B[0] += quadpts_arr[start]   * r_shape[ii]*z_shape[jj]*phi_shape[kk];
-                        B[1] += quadpts_arr[start+1] * r_shape[ii]*z_shape[jj]*phi_shape[kk];
-                        B[2] += quadpts_arr[start+2] * r_shape[ii]*z_shape[jj]*phi_shape[kk];
-
-                        is_lost = is_lost || (quadpts_arr[start+3] < 0); 
-                        // std::cout << ii << "\t" << jj << "\t" << kk << "\n";
-                        // std::cout << "interp surface dist val: " << quadpts_arr[start+3] << "\n";
-                        surface_dist += quadpts_arr[start+3] * r_shape[ii]*z_shape[jj]*phi_shape[kk];
-                    } else{
-                        // std::cout << "bad grid index for" << r << "\t" << phi << "\t" << z <<"\n"; 
-                    }
-
-                }
-            }
-        }
-        // std::cout << "B interpolated \n";
-
-        // std::cout << "r=" << r << "\t" << x << "\t" << y << "\t" << p.v_par << "\t" << surface_dist << "\n";
-        std::cout << t << "\t" << surface_dist << "\n";
-        p.has_left = (p.has_left || surface_dist <= 0);
-        if(p.has_left){
-            std::cout << "particle lost \n";
+        surface_dist = derivs[5];
+        if(surface_dist <= 0){
+            std::cout << "particle lost\n";
+            p.has_left = true;
             return;
         }
-        // std::cout << "particle not lost \n";
-
-        //  Interpolate grad B: columns are partial deriv wrt r, z, phi, rows are entries of B
-        //  row major order
-        for(int ii=0; ii<9; ++ii){
-            grad_B[ii] = 0.0;
-        }
-        dshape(r_rel, r_grid_size, r_dshape);
-        dshape(phi_rel, phi_grid_size, phi_dshape);
-        dshape(z_rel, z_grid_size, z_dshape);
-
-        for(int ii=0; ii<=3; ++ii){             
-            for(int jj=0; jj<=3; ++jj){                 
-                for(int kk=0; kk<=3; ++kk){
-                    int wrap_k = ((k+kk-1) % nphi) + 1;
-                    if ((i+ii >= 0 & i+ii < nr) & (j+jj >= 0 & j+jj < nz)){
-                        int start = 4*((i+ii)*nz*nphi + (j+jj)*nphi + (wrap_k));
-                        // interpolate gradient for each entry of B, filling in each column of the gradient
-                        for(int l=0; l<3; ++l){
-                            double Bval = quadpts_arr[start+l];
-                            grad_B[3*l]   += Bval * r_dshape[ii]*z_shape[jj]*phi_shape[kk];
-                            grad_B[3*l+1] += Bval * r_shape[ii]*z_dshape[jj]*phi_shape[kk];
-                            grad_B[3*l+2] += Bval * r_shape[ii]*z_shape[jj]*phi_dshape[kk];
-                        }
-                    }
-
-                }
-            }
-        }
-
-        // std::cout << "grad B interpolated \n";
-
-
-        // convert gradient from cylindrical (r, z, phi) to cartesian coordinates (x, y, z)
-        double c = cos(phi);
-        double s = sin(phi);
-
-
-        for(int l=0; l<3; ++l){ // iter over row
-            double dfdr = grad_B[3*l];
-            double dfdphi_divr = grad_B[3*l+2] / r;
-            
-            grad_B[3*l]   = c*dfdr - s*dfdphi_divr;
-            grad_B[3*l+2] = grad_B[3*l+1]; // z index changes
-            grad_B[3*l+1] = s*dfdr + c*dfdphi_divr;
-        }
-
-
-        // take ODE step
-
-        // std::cout << "starting updates \n";
-
-        double normB = sqrt(B[0]*B[0] + B[1]*B[1] + B[2]*B[2]);
-
-
-        // compute \nabla |B|
-        //  \nabla |B| = (\nabla B  B)(2 |B|)
-        nabla_normB[0] = (grad_B[0]*B[0] + grad_B[1]*B[1] + grad_B[2]*B[2]) / (2*normB);
-        nabla_normB[1] = (grad_B[3]*B[0] + grad_B[4]*B[1] + grad_B[5]*B[2]) / (2*normB);
-        nabla_normB[2] = (grad_B[6]*B[0] + grad_B[7]*B[1] + grad_B[8]*B[2]) / (2*normB);
-
-        // compute B \times \nabla |B|
-        cross_prod[0] = B[1]*nabla_normB[2] - B[2]*nabla_normB[1];
-        cross_prod[1] = B[2]*nabla_normB[0] - B[0]*nabla_normB[2];
-        cross_prod[2] = B[0]*nabla_normB[1] - B[1]*nabla_normB[0];
-
-
-        p.dotx = p.v_par * B[0]/normB + (0.5*pow(p.v_perp, 2) + pow(p.v_par, 2))*cross_prod[0] * m/(q*pow(normB, 3));
-        p.doty = p.v_par * B[1]/normB + (0.5*pow(p.v_perp, 2) + pow(p.v_par, 2))*cross_prod[1] * m/(q*pow(normB, 3));
-        p.dotz = p.v_par * B[2]/normB + (0.5*pow(p.v_perp, 2) + pow(p.v_par, 2))*cross_prod[2] * m/(q*pow(normB, 3));
-        
-        // record initial mu
-        if(t == 0.0){
-            mu = p.v_perp / (2*normB);
-        }
-
-        double BdotNablaNormB = B[0]*nabla_normB[0] + B[1]*nabla_normB[1] + B[2]*nabla_normB[2];
-        p.dotv_par = -mu*BdotNablaNormB;
-        p.v_perp = sqrt(2*mu*normB);
 
 
         // update
-        // std::cout << p.dotx << "\t" << p.doty << "\t" << p.dotz << "\t" << p.dotv_par << "\n";
-        p.x += p.dotx * dt;
-        p.y += p.doty * dt;
-        p.z += p.dotz * dt;
-        p.v_par += p.dotv_par * dt;
+        std::cout << "x update: " << p.x << "\t" <<  derivs[0] <<  "\t" << derivs[0] * dt << "\n";
+        p.x += derivs[0] * dt;
+        p.y += derivs[1] * dt;
+        p.z += derivs[2] * dt;
+        p.v_par += derivs[3] * dt;
+        p.v_perp = sqrt(2*mu*derivs[4]);
 
         t += dt;
         // std::cout << "updates complete \n";
