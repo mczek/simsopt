4b26f6cb201234b2c772855131b1636c555f93a7
mczekanski1 <38838492+mczek@users.noreply.github.com>
2025-09-25 08:32:49 -0400

    add gpu examples

--- examples/4_GPU/boozer_poincare.py
+++ examples/4_GPU/boozer_poincare.py
@@ -52,42 +52,68 @@ thetarange = (0, np.pi, 15)
 zetarange = (0, 2*np.pi/nfp, 15)
 field = InterpolatedBoozerField(bri, degree, srange, thetarange, zetarange, True, nfp=nfp, stellsym=True)
 
+### Maximum tracing time
+tmax = 1e-4
+
+### Sample initial conditions
+# nparticles = 1000
+# stz = np.vstack([sample_stz(field, maxJ) for _ in range(nparticles)])
+s_vals = np.linspace(0, 1, 10)
+theta_vals = np.linspace(-np.pi, np.pi, 10)
+stz = np.vstack([[s_val, theta_val, 0] for s_val in s_vals for theta_val in theta_vals])
+
+nparticles = stz.shape[0]
+vpar_init = np.sqrt(2*1e3*ONE_EV/MASS)* 0.5*np.ones(nparticles)#np.random.uniform(-1, 1, nparticles)
+
+
+### First, get poincare data from simsopt
+
+# # x is an element of gc_zeta_hits
+# # we want to make a dataframe with the particle location
+# # at each hit
+# def get_position_data(x):
+
+
+# trace this particle in simsopt, saving the trajectory
+# print("simsopt tracing...")
+# gc_tys, gc_zeta_hits = trace_particles_boozer(
+#     field, stz, vpar_init, tmax=tmax, mass=MASS, charge=CHARGE,
+#     Ekin=1e3*ONE_EV, zetas=[0], tol=1e-9, stopping_criteria=[MaxToroidalFluxStoppingCriterion(0.99)],
+#     forget_exact_path=True)
+
+# # print(gc_zeta_hits)
+# rows = []
+# for i in range(len(gc_zeta_hits)):
+#     if len(gc_zeta_hits[i]) > 0:
+#         # print(gc_zeta_hits[i])
+#         loc = gc_zeta_hits[i][:, 2:5]
+#         n_hits = loc.shape[0]
+#         rows += [np.hstack((loc, i*np.ones((n_hits,1))))]
+# full_data = np.vstack(rows)
+# simsopt_poincare_data = pd.DataFrame({'x1': full_data[:, 0], 'x2': full_data[:, 1], 'zeta': full_data[:, 2], "id":full_data[:,3]})
+# simsopt_poincare_data['s'] = np.sqrt(simsopt_poincare_data['x1']**2 + simsopt_poincare_data['x2']**2)
+# simsopt_poincare_data['theta'] = np.arctan2(simsopt_poincare_data['x2'], simsopt_poincare_data['x1'])
+# simsopt_poincare_data.to_csv("examples/4_GPU/cpu_boozer_poincare.csv")
+
+# exit()
+
+### Now create poincare data with GPU tracing
+### using bisection algorithm
+
+# create gpu interpolant data
 srange, trange, zrange, quad_info, maxJ = boozer_interpolant(field, nfp, 15)
 
-# pick tmax and a single particle
-tmax = 1e-4
-dt_save = 1e-6
-nparticles = 1000
-stz = np.vstack([sample_stz(field, maxJ) for _ in range(nparticles)])
-vpar_init = np.sqrt(2*1e3*ONE_EV/MASS)*np.random.uniform(-1, 1, nparticles)
-
-cpu_stz = stz.copy()
-cpu_vpar = vpar_init.copy()
+# First set an initial distance at which to save
+dt_save = 1e-7
 
 gpu_stz = stz.copy()
 gpu_vpar = vpar_init.copy()
 
-tracing_time = 0
+gpu_time = 0
 snapshots = []
-while tracing_time < tmax:
-    print(tracing_time)
-
-    # trace this particle in simsopt, saving the trajectory
-    gc_tys, gc_zeta_hits = trace_particles_boozer(
-        field, cpu_stz, cpu_vpar, tmax=dt_save, mass=MASS, charge=CHARGE,
-        Ekin=1e3*ONE_EV, tol=1e-9, stopping_criteria=[MaxToroidalFluxStoppingCriterion(0.99)],
-        forget_exact_path=True)
-
-    cpu_final_stz = np.vstack([gc_tys[i][-1][1:4] for i in range(nparticles)])
-
-    # trace_particles_boozer leaves positions in cylindrical coordinates
-    x1 = cpu_final_stz[:, 0]
-    x2 = cpu_final_stz[:, 1]
-    cpu_final_stz[:, 0] = np.sqrt(x1*x1 + x2*x2)
-    cpu_final_stz[:, 1] = np.arctan2(x2, x1)
-
-    cpu_final_vpar = np.vstack([gc_tys[i][-1][4] for i in range(nparticles)])
-    cpu_final_t = np.vstack([gc_tys[i][:, 0] for i in range(nparticles)])
+while gpu_time < tmax:
+    print(gpu_time)
+    gpu_time += dt_save
 
     last_time = sopp.boozer_gpu_tracing(
         quad_pts=quad_info, 
@@ -105,6 +131,49 @@ while tracing_time < tmax:
         nparticles=nparticles)
 
     last_time = np.reshape(last_time, (nparticles, 7))
+
+    # save snapshot s, theta, zeta, vpar
+    snapshot_data = pd.DataFrame({'s':last_time[:, 0],
+                                  'theta':last_time[:,1],
+                                  'zeta':last_time[:,2],
+                                  'vpar':last_time[:,3]})
+    snapshot_data['time'] = gpu_time
+    snapshot_data['id'] = list(range(nparticles))
+    snapshots += [snapshot_data]
+
+full_snapshots = pd.concat(snapshots, ignore_index=True)
+full_snapshots = full_snapshots.sort_values(by = ['id', 'time'])
+# check for poincare punctures
+ids = full_snapshots['id']
+zetas = full_snapshots['zeta']
+zetas_shift = full_snapshots - np.pi
+nrows = full_snapshots.shape[0]
+zeta_crossing = [np.cos(zetas[i]) > 0 and np.sign(zetas_shift[i]) != np.sign(zetas_shift[i+1]) for i in range(nrows-1)]
+
+need_refine = [ids[i] == ids[i+1] and np.sign(zetas[i]) != np.sign(zetas[i+1]) for i in range(nrows-1)]
+
+
+print(full_snapshots)
+print(need_refine) 
+print(np.sum(need_refine))
+exit()
+
+gpu_stz = stz.copy()
+cpu_vpar = vpar_init.copy()
+
+gpu_stz = stz.copy()
+gpu_vpar = vpar_init.copy()
+
+tracing_time = 0
+snapshots = []
+while tracing_time < tmax:
+    print(tracing_time)
+
+    cpu_final_stz = np.vstack([gc_tys[i][-1][1:4] for i in range(nparticles)])
+    cpu_final_vpar = np.vstack([gc_tys[i][-1][4] for i in range(nparticles)])
+    cpu_final_t = np.vstack([gc_tys[i][:, 0] for i in range(nparticles)])
+
+
 
 
     snapshot_data = pd.DataFrame({'s_start' : stz[:,0], "theta_start": stz[:, 1], "zeta_start":stz[:,2], 'vpar_start':vpar_init,
@@ -135,8 +204,7 @@ output_data['gpu_x2'] = output_data['gpu_s_end']*np.sin(output_data['gpu_theta_e
 
 output_data['x1_diff'] = output_data['cpu_x1'] - output_data['gpu_x1']
 output_data['x2_diff'] = output_data['cpu_x2'] - output_data['gpu_x2']
-output_data['zeta_diff'] = np.mod(output_data['cpu_zeta_end'] - output_data['gpu_zeta_end'], 2*np.pi)
-output_data['zeta_diff'] = np.where(output_data['zeta_diff'] > np.pi, 2*np.pi - output_data['zeta_diff'], output_data['zeta_diff'])
+output_data['zeta_diff'] = output_data['cpu_zeta_end'] - output_data['gpu_zeta_end']
 output_data['dist'] = np.sqrt(output_data['x1_diff']**2 + output_data['x2_diff']**2 + output_data['zeta_diff']**2)
 
 output_data.to_csv("./boozer_device_comparison.csv")
---
+++ examples/4_GPU/gpu_cartesian_tracing.py
@@ -0,0 +1,167 @@
+import simsoptpp as sopp
+from simsopt.field import (BoozerRadialInterpolant, InterpolatedBoozerField, trace_particles,
+                           MinToroidalFluxStoppingCriterion, MaxToroidalFluxStoppingCriterion,
+                           ToroidalTransitStoppingCriterion, IterationStoppingCriterion, compute_resonances)
+from simsopt.mhd import Vmec
+import numpy as np
+from simsopt.util.constants import (
+        ALPHA_PARTICLE_MASS as MASS,
+        FUSION_ALPHA_PARTICLE_ENERGY as ENERGY,
+        ALPHA_PARTICLE_CHARGE as CHARGE
+        )
+from simsopt.field.sampling import draw_uniform_on_surface
+from simsopt.geo import SurfaceRZFourier, SurfaceXYZTensorFourier, curves_to_vtk, BoozerSurface, Volume
+from simsopt.util import in_github_actions, proc0_print
+
+import os
+import pandas as pd
+
+from simsopt._core import load
+from simsopt.configs import get_ncsx_data
+from simsopt.field import BiotSavart, coils_via_symmetries, InterpolatedField
+from simsopt.geo import SurfaceRZFourier, SurfaceClassifier
+from simsopt.util import cartesian_interpolant  
+
+
+np.random.seed(1800)
+tmax = 1e-3
+nparticles = 10000
+n_metagrid_pts = 30
+
+### CREATE A FIELD FOR TRACING
+ID = 1655332
+fID = ID // 1000
+[surfaces, ma, coils] = load("./serial1655332.json")
+nfp = surfaces[0].nfp
+nc_per_hp = len(coils)//nfp//2
+base_coils = coils[:nc_per_hp]
+base_curves = [c.curve for c in base_coils]
+
+# compute half radius surface
+mpol = surfaces[0].mpol
+ntor = surfaces[0].ntor
+stellsym = surfaces[0].stellsym
+nfp = surfaces[0].nfp
+phis = np.linspace(0, 1/nfp, 2*mpol+1, endpoint=False)
+thetas=np.linspace(0, 1, 2*ntor+1, endpoint=False)
+surface_hr = SurfaceXYZTensorFourier(mpol=mpol, ntor=ntor, nfp=nfp, quadpoints_phi=phis, quadpoints_theta=thetas, stellsym=stellsym)
+surface_hr.x = surfaces[0].x
+mR = surfaces[-1].minor_radius()
+bs_bs = BiotSavart(coils)
+label = Volume(surface_hr)
+targetlabel = np.sign(surface_hr.volume()) * 2*np.pi * 1.0 * (np.pi * (mR/2)**2)
+boozer_surface = BoozerSurface(bs_bs, surface_hr, label, targetlabel)
+
+# df = pd.read_pickle('/mnt/home/mczekanski/ceph/QUASR_08072024/QUASR_08072024.pkl')
+# iota0 = df[df.ID==ID].iloc[0].mean_iota
+iota0 = 1.2
+print("iota = ", iota0)
+coil_currents = [c.current.get_value() for c in coils]
+G0 = 2. * np.pi * np.sum(np.abs(coil_currents)) * (4 * np.pi * 10**(-7) / (2 * np.pi))
+res = boozer_surface.solve_residual_equation_exactly_newton(tol=1e-10, maxiter=20, iota=iota0, G=G0)
+
+if res['success']:
+    proc0_print('HALF RADIUS COMPUTATION SUCCESS', flush=True)
+else:
+    proc0_print('HALF RADIUS COMPUTATION FAILURE', flush=True)
+    quit()
+
+# RESCALE TO ARES-CS MINOR RADIUS
+mR = surfaces[-1].minor_radius()
+scale = 1.7 / mR
+for s in surfaces + [surface_hr]:
+    s.x = scale*s.x
+for c in base_curves:
+    c.x = scale*c.x
+# ma.x = scale * ma.x
+
+# MAKE THE MAGNETIC FIELD ON AXIS 5.685257882303897
+bs = BiotSavart(coils)
+meanB = np.mean(bs.set_points(ma.gamma()).AbsB())
+
+scale = 5.685257882303897/meanB
+for c in base_coils:
+    c.current.x = scale*c.current.x
+
+
+curves = [c.curve for c in coils]
+s_hp = surfaces[-1]
+s_outer = SurfaceXYZTensorFourier(mpol=s_hp.mpol, ntor=s_hp.ntor, stellsym=s_hp.stellsym, nfp=nfp,\
+        quadpoints_phi=np.linspace(0, 1, 256, endpoint=False), quadpoints_theta=np.linspace(0, 1, 256, endpoint=False))
+s_outer.x = s_hp.x
+
+# create loss surface
+sc_particle = SurfaceClassifier(s_outer, h=0.1, p=2)
+
+# sample initial conditions
+rs = np.linalg.norm(s_outer.gamma()[:, :, 0:2], axis=2)
+zs = s_outer.gamma()[:, :, 2]
+rrange = (np.min(rs), np.max(rs), n_metagrid_pts)
+phirange = (0, 2*np.pi/nfp, n_metagrid_pts)
+# exploit stellarator symmetry and only consider positive z values:
+zrange = (0, np.max(zs), n_metagrid_pts)
+print(rrange, phirange, zrange)
+field = InterpolatedField(
+        bs, 3, rrange, phirange, zrange, True, nfp=nfp, stellsym=True
+)
+
+
+### SAMPLE ICs
+s_spawn_coarse = boozer_surface.surface
+s_spawn_fine = SurfaceXYZTensorFourier(mpol=s_spawn_coarse.mpol, ntor=s_spawn_coarse.ntor, \
+        stellsym=s_spawn_coarse.stellsym, nfp=s_spawn_coarse.nfp,\
+        quadpoints_phi=np.linspace(0, 1/s_spawn_coarse.nfp, 1000, endpoint=False), \
+        quadpoints_theta=np.linspace(0, 1, 1000, endpoint=False))
+s_spawn_fine.x = s_spawn_coarse.x
+
+xyz_init, _ = draw_uniform_on_surface(s_spawn_fine, nparticles, safetyfactor=10)
+
+VELOCITY = np.sqrt(2 * ENERGY / MASS)
+vpar_init = np.random.uniform(-VELOCITY, VELOCITY, (nparticles,))
+
+xyz_init = np.ascontiguousarray(xyz_init)
+vpar_init = np.ascontiguousarray(vpar_init)
+
+
+### SIMSOPT TRACING
+# print("simsopt tracing")
+# gc_tys, gc_phi_hits = trace_particles(
+#         field, xyz_init, vpar_init, tmax=tmax, mass=MASS, charge=CHARGE,
+#         Ekin=ENERGY, tol=1e-9, phis=[],
+#         stopping_criteria=[LevelsetStoppingCriterion(sc_particle.dist)], mode='gc_vac', forget_exact_path=True,
+#         phase_angle=0)
+
+# cpu_last_time = [trajectory[-1][0] for trajectory in gc_tys]
+# cpu_last_x = [trajectory[-1][1] for trajectory in gc_tys]
+# cpu_last_y = [trajectory[-1][2] for trajectory in gc_tys]
+# cpu_last_z = [trajectory[-1][3] for trajectory in gc_tys]
+# cpu_last_vpar = [trajectory[-1][4] for trajectory in gc_tys]
+
+
+
+### SETUP NEW INTERPOLANT
+r_range, phi_range, z_range, quad_info = cartesian_interpolant(field, sc_particle, nfp, n_metagrid_pts)
+
+### GPU TRACING
+print("gpu tracing")
+last_time = sopp.cartesian_gpu_tracing(quad_info, r_range, phi_range, z_range, xyz_init, MASS, CHARGE, VELOCITY, vpar_init, tmax, 1e-9, nparticles)
+
+
+last_time = np.reshape(last_time, (nparticles, 7))
+
+
+# particle_data = pd.DataFrame({'x_start': xyz_init[:,0], 'y_start': xyz_init[:,1], 'z_start':xyz_init[:,2], 'vpar_start':vpar_init,
+# 							  'gpu_x_end': last_time[:,0], 'cpu_x_end':cpu_last_x,
+#                               'gpu_y_end':last_time[:,1], 'cpu_y_end':cpu_last_y,
+#                               'gpu_z_end':last_time[:,2], 'cpu_z_end':cpu_last_z,
+#                               'gpu_vpar_end':last_time[:,3], 'cpu_vpar_end':cpu_last_vpar,
+#                               'gpu_last_time':last_time[:,4], 'cpu_last_time' : cpu_last_time,
+# 							  'gpu_steps_accepted':last_time[:,5], 'gpu_steps_attempted':last_time[:,6]})
+particle_data = pd.DataFrame({'x_start': xyz_init[:,0], 'y_start': xyz_init[:,1], 'z_start':xyz_init[:,2], 'vpar_start':vpar_init,
+							  'gpu_x_end': last_time[:,0],
+                              'gpu_y_end':last_time[:,1],
+                              'gpu_z_end':last_time[:,2],
+                              'gpu_vpar_end':last_time[:,3],
+                              'gpu_last_time':last_time[:,4],
+							  'gpu_steps_accepted':last_time[:,5], 'gpu_steps_attempted':last_time[:,6]})
+particle_data.to_csv('cartesian_particle_data.csv')
